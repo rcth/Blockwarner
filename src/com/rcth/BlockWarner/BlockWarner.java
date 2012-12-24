2173d39b4f0a1eb2b2c4bf43f9d638f76607eb36
rcth <rcthcraft@gmail.com>
2012-12-24 10:27:50 +0100

    Working on config file

--- src/com/rcth/BlockWarner/BlockWarner.java
+++ src/com/rcth/BlockWarner/BlockWarner.java
@@ -2,8 +2,16 @@ package com.rcth.BlockWarner;
 
 import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public final class BlockWarner extends JavaPlugin {
+	
     
 	@Override
 	public void onEnable() {
